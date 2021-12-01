a74f9f958d0dd87b550dbf2126825a1f95f16f54
akayee <53110296+akayee@users.noreply.github.com>
2021-12-01 19:46:25 +0300

    Resimler eklend,

--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -11,6 +11,18 @@ import saglikbakanligi from '../assets/projeler/saglikbakanligi.jpg';
 import beonfabrikasi from '../assets/projeler/beonfabrikasi.jpg';
 import tatlar from '../assets/projeler/tatlar.jpg';
 import yuzengesjaponya from '../assets/projeler/yuzengesjaponya.jpg';
+import CatiGes1 from '../assets/projeler/Catiges14.jpeg'
+import Catiges2 from '../assets/projeler/Catiges2.jpeg'
+import Catiges4 from '../assets/projeler/Catiges4.jpeg'
+import Catiges5 from '../assets/projeler/Catiges5.jpeg'
+import Catiges6 from '../assets/projeler/Catiges6.jpeg'
+import Catiges7 from '../assets/projeler/Catiges7.jpeg'
+import Inverter9 from '../assets/projeler/Inverter9.jpeg'
+import Kablokurulum3 from '../assets/projeler/Kablokurulum3.jpeg'
+import Trafo8 from '../assets/projeler/Trafo8.jpeg'
+import Zeminges10 from '../assets/projeler/Zeminges10.jpeg'
+import Zeminges12 from '../assets/projeler/Zeminges12.jpeg'
+import Catiges13 from '../assets/projeler/Catiges13.jpeg'
 
 const projects = [{
     adi: "STM ve DSİ GES Projesi",
@@ -29,80 +41,81 @@ const projects = [{
 {
     adi: "Aksaray Çatı GES",
     summary: "1 MW",
-    img: ankaracevrekoruma
+    img: CatiGes1
 },
 {
     adi: "Aksaray Çatı GES",
     summary: "350 kW",
-    img: caykur
+    img: Catiges4
 },
 {
     adi: "Aksaray Çatı GES",
     summary: "300 kW",
-    img: saglikbakanligi
+    img: Catiges13
 },
 {
     adi: "Aksaray Arazi GES",
     summary: "5 MW.",
-    img: beonfabrikasi
+    img: Catiges2
 },
 {
     adi: "Bursa Arazi GES",
     summary: "5 MW",
-    img: tatlar
+    img: ankaracevrekoruma
 },
 {
     adi: "İzmir Arazi GES",
     summary: "10 MW",
-    img: tatlar
+    img: caykur
 },
 {
     adi: "Konya Arazi GES",
     summary: "15 MW",
-    img: tatlar
+    img: Zeminges10
 },
 {
     adi: "Hatay Arazi GES",
     summary: "10 MW",
-    img: tatlar
+    img: Zeminges12
 },
 {
     adi: "Nevşehir Soğuk Hava Çatı GES",
     summary: "2.5 MW",
-    img: tatlar
+    img: Catiges5
 },
 {
     adi: "İzmir Çatı GES",
     summary: "5 MW",
-    img: tatlar
+    img: Catiges6
 },
 {
     adi: "Kırşehir Arazi GES",
     summary: "4.5 MW",
-    img: tatlar
+    img: Catiges7
 },
 {
     adi: "Düzce Çatı GES",
     summary: "2.5 MW",
-    img: tatlar
+    img: Inverter9
 },
 {
     adi: "Düzce Çatı / Arazi GES",
     summary: "15 MW",
-    img: tatlar
+    img: Trafo8
 }
 ];
 function Projects(props) {
 
     const renderItem = (index, key) => {
-        return <Col md="4" sm="12" key={key} className={styles.projectItem}>
+        return <Col md="6" sm="12" lg="3" key={key} className={styles.projectItem}>
             <div>
+                <img className={styles.projectImage} src={projects[key].img} />
                 <p className={styles.projecTitle}>{projects[key].adi}</p>
             </div>
             <div>
                 <p className={styles.projecSummarry}> {projects[key].summary}</p>
             </div>
-            
+
         </Col>;
     }
 
@@ -117,7 +130,6 @@ function Projects(props) {
             </Row>
         </div>
 
-        <a href="https://www.freepik.com/photos/green">Green photo created by wirestock - www.freepik.com</a>
     </div>
 };
 
