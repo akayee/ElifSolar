5101408c7d8e90b2946c04c139ccf8060403a4d9
akayee <53110296+akayee@users.noreply.github.com>
2021-12-15 08:46:49 +0300

    tailwinde renk eklendi

--- src/screens/Anasayfa.js
+++ src/screens/Anasayfa.js
@@ -21,19 +21,34 @@ function Anasayfa(props) {
     }}>
 
         <Container >
-            <h1 className={styles.bigblue}>Elif Global Solar Güneş Enerjisi Sistemleri</h1>
-            
+            <h1 className="text-lime text-5xl font-semibold text-center">Elif Global Solar Güneş Enerjisi Sistemleri</h1>
+
             <Col sm="6">
-                <div className={styles.anasayfaCard}>
-                    Uzman kadromuz ve tecrübeli iş ortaklarımızdan yararlanmak için teklif alın.
+                <div >
+                    <p className="text-lime">
+                        Uzman kadromuz ve tecrübeli iş ortaklarımızdan yararlanmak için teklif alın.
+                    </p>
+
                     <div>
 
                     </div>
-                    <div className={styles.anasayfaDuyuru}>
-                        Devlet KDV Desteği ve Fazla Üretimde Alım Garantisi Fırsatlarını Kaçırmayın.
+                    <div className="max-w-md py-4 px-8 bg-white/[60] shadow-lg rounded-lg my-20 ">
+                        <h2 className="text-lime text-3xl font-semibold">
+                            Garantili Satış
+                        </h2>
+                        <p className="mt-2 text-gray-400 font-serif">
+                            Devlet KDV Desteği ve Fazla Üretimde Alım Garantisi Fırsatlarını Kaçırmayın.
+                        </p>
+
                     </div>
-                    <div className={styles.anasayfaDuyuru}>
-                        Tarım Bakanlığı Hibesi dahil olmak üzere anahtar teslim projelendirme.
+                    <div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20 ">
+                        <h2 className="text-lime text-3xl font-semibold">
+                            Tarım Bakanlığı Desteği
+                        </h2>
+                        <p className="mt-2 text-gray-400 font-serif">
+                            Tarım Bakanlığı %50'ye kadar hibe desteği sunmaktadır. Bu hibe süreci takibi de dahil olmak üzere anahtar teslim projelendirme.
+                        </p>
+
                     </div>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors:{
+      'lime':'#65a30d'
+    }
   },
   plugins: [],
 }
